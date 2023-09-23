1dd25f626263a0ffead91f197778af9789b6cf7e
Dmitriy Solk <nesolk13@gmail.com>
2023-09-23 19:11:46 +0300

    New fetcher

--- internal/storage/source.go
+++ internal/storage/source.go
@@ -16,10 +16,9 @@ func NewSourceStorage(db *sqlx.DB) *SourcePostgresStorage {
 	return &SourcePostgresStorage{db: db}
 }
 
-
 func (s *SourcePostgresStorage) Sources(ctx context.Context) ([]model.Source, error) {
 	conn, err := s.db.Connx(ctx)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
@@ -32,15 +31,15 @@ func (s *SourcePostgresStorage) Sources(ctx context.Context) ([]model.Source, er
 	return lo.Map(sources, func(source dbSource, _ int) model.Source { return model.Source(source) }), nil
 }
 
-///////////////////////////////
-
 func (s *SourcePostgresStorage) SourceByID(ctx context.Context, id int64) (*model.Source, error) {
 	conn, err := s.db.Connx(ctx)
-	if err != nil
+	if err != nil {
+		return nil, err
+	}
 	defer conn.Close()
 
 	var source dbSource
-	if err := conn.GetContext(ctx, &source, `SELECT * FROM sources WHERE id = $1`, id); err != nil{
+	if err := conn.GetContext(ctx, &source, `SELECT * FROM sources WHERE id = $1`, id); err != nil {
 		return nil, err
 	}
 	return (*model.Source)(&source), nil
@@ -57,10 +56,10 @@ func (s *SourcePostgresStorage) Add(ctx context.Context, source model.Source) (i
 
 	row := conn.QueryRowContext(
 		ctx,
-		`INSERT INTO sources (name, feed_url, priority)
-									VALUES ($1,$2,$3) RETURNING id;`,
-				source.Name, source.FeedURL, source.Priority,
-		)
+		`INSERT INTO sources (name, feed_url, priority) 
+								VALUES ($1,$2,$3) RETURNING id;`,
+		source.Name, source.FeedURL, source.Priority,
+	)
 
 	if err := row.Err(); err != nil {
 		return 0, err
@@ -99,9 +98,9 @@ func (s *SourcePostgresStorage) Delete(ctx context.Context, id int64) error {
 }
 
 type dbSource struct {
-	ID      	int64  `db:"id"`
-	Name    	string `db:"name"`
-	FeedURL   	string `db:"feed_url"`
-	Priority  	int       `db:"priority"`
-	CreatedAt 	time.Time `db:"created_at"`
+	ID        int64     `db:"id"`
+	Name      string    `db:"name"`
+	FeedURL   string    `db:"feed_url"`
+	Priority  int       `db:"priority"`
+	CreatedAt time.Time `db:"created_at"`
 }
